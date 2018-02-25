b86f6cfa7ef6b261f2550d24f5ce73a3e2d10aeb
Peter <softforte@gmail.com>
2018-02-25 17:13:48 +0800

    Configured JS and CSS for our app, and set up database config settings

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,15 +8,9 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Buery from 'buefy';
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.use(Buery);
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+//Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
